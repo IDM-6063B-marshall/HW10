ef40097d856494002d54047c32fff1f1cafd1386
Jubilee Marshall <jam1703@nyu.edu>
2024-11-14 10:10:25 -0500

    finalized sketch A

--- 0/sketch.js
+++ 0/sketch.js
@@ -30,15 +30,20 @@ function draw() {
   noStroke();
   mFFT.analyze();
 
-  // Bass (darkest color)
+  // Set up the grid for the circles (mapped box in the center)
+  let boxWidth = 600; // Width of the box
+  let boxHeight = 400; // Height of the box
+  let boxX = (width - boxWidth) / 2; // Centered horizontally
+  let boxY = (height - boxHeight) / 2; // Centered vertically
+
+  // Bass (darkest color brown)
   push();
   fill(54, 34, 20);
   let energyB = mFFT.getEnergy("bass");
-  let diamB = map(energyB, 0, 255, 0, 100); // Smaller diameter for bass circles to stop this from inducing seizures lol
+  let diamB = map(energyB, 0, 255, 0, 100); // Smaller diameter for bass circles
 
-  for (let y = 50; y < height + 500; y += 150) {
-    // More spaced out
-    for (let x = 50; x < width + 500; x += 150) {
+  for (let y = boxY; y < boxY + boxHeight; y += 100) {
+    for (let x = boxX; x < boxX + boxWidth; x += 100) {
       ellipse(x, y, diamB);
     }
   }
@@ -50,8 +55,8 @@ function draw() {
   let energyM = mFFT.getEnergy("mid");
   let diamM = map(energyM, 0, 255, 0, 100); // Smaller diameter for mid-range circles
 
-  for (let y = 50; y < height + 500; y += 150) {
-    for (let x = 50; x < width + 500; x += 150) {
+  for (let y = boxY; y < boxY + boxHeight; y += 100) {
+    for (let x = boxX; x < boxX + boxWidth; x += 100) {
       ellipse(x, y, diamM);
     }
   }
@@ -63,40 +68,45 @@ function draw() {
   let energyT = mFFT.getEnergy("treble");
   let diamT = map(energyT, 0, 255, 0, 100); // Smaller diameter for treble circles
 
-  for (let y = 50; y < height + 500; y += 150) {
-    for (let x = 50; x < width + 500; x += 150) {
+  for (let y = boxY; y < boxY + boxHeight; y += 100) {
+    for (let x = boxX; x < boxX + boxWidth; x += 100) {
       ellipse(x, y, diamT);
     }
   }
   pop();
 
-  //STILL NEED TO DO VOLUME PART
-  // let vol = mAmplitude.getLevel();
-
-  // let diam = map(vol, 0, 1, 0, 1.4 * width);
-  // let sat = map(vol, 0, 1, 55, 255);
-
-  // let c = color(0, 0, sat, sat);
-
-  // fill(c);
-  // ellipse(width / 2, height / 2, diam);
-
   // coffee mug
-  // stole from https://editor.p5js.org/emayne/sketches/eufFc7M05
-  // push();
-  // fill(56, 18, 34);
-  // translate(windowWidth / 2, windowHeight / 2);
-  // rect(180, 200, 250, 200);
-  // arc(305, 400, 250, 150, (4 * PI) / 2, (2 * PI) / 2);
-  // ellipse(420, 300, 170, 200);
-  // arc(305, 210, 250, 150, (2 * PI) / 2, (4 * PI) / 2);
+  // code snagged from https://editor.p5js.org/emayne/sketches/eufFc7M05
+  push();
+  noStroke();
+  fill(56, 18, 34);
+  translate(windowWidth / 2, windowHeight / 2.5);
+  rect(180, 200, 250, 200);
+  arc(305, 400, 250, 150, (4 * PI) / 2, (2 * PI) / 2);
+  ellipse(420, 300, 170, 200);
+  arc(305, 210, 250, 150, (2 * PI) / 2, (4 * PI) / 2);
 
-  // fill(248, 244, 227)
-  // arc(430, 300, 110, 140, (3 * PI) / 2, PI / 2);
+  fill(bgRed, bgGreen, bgBlue);
+  arc(430, 300, 110, 140, (3 * PI) / 2, PI / 2);
 
-  // fill(248, 244, 227);
-  // ellipse(305, 220, 215, 115);
-  // pop();
+  fill((169 + 54) / 2, (124 + 34) / 2, (115 + 20) / 2);
+  ellipse(305, 220, 215, 105);
+  pop();
+
+  let vol = mAmplitude.getLevel();
+  let steam = map(vol, 0, 1, 0, 255); // transparency of steam based on vol
+
+  fill(248, 244, 227, steam);
+  strokeWeight(15);
+
+  for (let i = 0; i < 3; i++) {
+    beginShape();
+    for (let y = 450; y < 650; y++) {
+      let x = 1000 + i * 60 + sin(y * 0.05) * 10; // Sine wave for horizontal movement
+      vertex(x - 40, y - 75);
+    }
+    endShape();
+  }
 }
 
 function mouseClicked() {
